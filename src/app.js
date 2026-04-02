8a9d97499bad76b9859a3186d9fea131ce125eb7
sachinkumar <sachinkumarmd15@gmail.com>
2026-04-02 12:06:46 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user", (req, res) => {
+// app.get("/user", (req, res) => {
+//     console.log(req.query)
+//     res.send({firstName: "Sachinkumar", lastName: "Doddamani"})
+// });
+
+app.get("/user/:userId/:password", (req, res) => {
+    console.log(req.params)
     res.send({firstName: "Sachinkumar", lastName: "Doddamani"})
 });
-
-app.post("/user", (req, res) => {
-    console.log("Save data to the database"); //assume saving data to DB
-    res.send("Data successfully saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("Data successfully deleted");
-});
-
-
-//this will match with all HTTP methods API calls to /test
-app.use("/test", (req, res) => {
-    res.end("Hello from the Server!")
-})
 
 app.listen(7777,  () => {
     console.log("Server is successfully listening on port 7777...")
