906ca437ff4b869efad3f37504f3f9c4b6a26d6b
sachinkumar <sachinkumarmd15@gmail.com>
2026-04-02 12:45:59 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,15 +2,35 @@ const express = require('express');
 
 const app = express();
 
-// app.get("/user", (req, res) => {
-//     console.log(req.query)
-//     res.send({firstName: "Sachinkumar", lastName: "Doddamani"})
-// });
+// app.use("/user", [rH, rH2, rH3, rH4, rH5]); //wrapping route hanlders in an array
+// app.use("/user", rH, [rH2, rH3], rH4, rH5);
 
-app.get("/user/:userId/:password", (req, res) => {
-    console.log(req.params)
-    res.send({firstName: "Sachinkumar", lastName: "Doddamani"})
-});
+app.use("/user", (req, res, next) => {
+    console.log("Handling the route user!");
+    // res.send("Response!!")
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 2!");
+    // res.send("2nd Response!!")
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 3!");
+    // res.send("3rd Response!!");
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 4!");
+    // res.send("4th Response!!")
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 4!");
+    res.send("5th Response!!")
+}
+
+)
 
 app.listen(7777,  () => {
     console.log("Server is successfully listening on port 7777...")
